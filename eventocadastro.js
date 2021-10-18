d02fec6600d24c314e7cff5f45b1d28f83126652
estranhoger <estranhohelp1@gmail.com>
2021-10-18 15:26:22 -0300

    cod update
    Modificação dos cadastros em geral

---
+++ eventocadastro.js
@@ -0,0 +1,34 @@
+console.log("hello world!");
+let data_evento = new Date (2021,11,20);
+
+let data_submissao = new Date (2021,11,23);
+
+if( data_submissao <= data_evento) {
+    console.log("Inscricao em analise!")
+} else{ 
+    console.log("inscricao recusada!Fora do período de submissão")
+}
+
+
+let inscrição_idade = 16;
+
+
+if(inscrição_idade >= 18) { 
+    console.log("Cadastro Autorizado!")
+} else { 
+    console.log("Cadastro recusado, idade mínima não atingida!")
+}
+
+let lista_participantes_nome = 104
+for (contador = 1 ; contador <=101 ; contador ++ ) {
+
+} 
+
+if (lista_participantes_nome <=101) { 
+    console.log ("Sejam bem vindos ao evento!")
+} else {
+    console.log ("Infezlimente as vagas estão completas, não será possível participar deste evento.")
+ }
+    
+
+
